8bfe24d35290497dbbb041e6c47a70f2154d2b13
Elene1029 <garibovielene69@gmail.com>
2024-07-14 01:08:57 +0400

    second commit

---
+++ Day(07)/Classwork.py
@@ -0,0 +1,9 @@
+#მომხმარებელს input-ის გამოყენებით უნდა შემოატანინოთ 2 რიცხვი და შმდეგ ამ 2 რიხვით უნდა გააკეთოთ მათემატიკური მაგალითები/ოპერაციები.
+
+num1 = int(input("enter numbber: "))
+num2 = int(input("enter next numbber: "))
+
+print(num1 - num2)
+print(num1 + num2)
+print(num1 * num2)
+print(num1 // num2)
---
+++ Day(07)/Classwork1.py
@@ -0,0 +1,12 @@
+#type()-ტიპი
+
+car = "porsh"
+
+print(type(car))
+
+a = 4
+b = 2
+c = 4/2
+print(c) #displays the result
+print(type(c)) # displays data type
+
