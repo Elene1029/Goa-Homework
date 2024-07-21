b8ee602e8eb51bf1e30af25340aaba1bbd754c6a
Elene1029 <garibovielene69@gmail.com>
2024-07-22 03:09:17 +0400

    second commit

---
+++ Day(08)/Classwork.py
@@ -0,0 +1,19 @@
+# Compare - შედარება
+# Comparison operations - შედარებითი ოპერაცია, სიმბოლოები: < ; >
+# true - ჭეშმარიტი/სწორი
+# false - მცდარი/არასწირე
+#boolean - დატას ტიპი, აქვს მნიშვნელობა ჭეშმარიტი ან მცდარი (მხოლოდ ეს ორი) true/false
+print(5 < 9)    # True
+print(5 > 9)    # False
+print("7")      # str
+print(7)        # int
+print(7.0)      # float
+
+#statement - სტატია/სთქმულება
+#circut - წრედი
+#binary code system = ორობითი სისტემა ex:0101010101001
+#morse code (პროგრამირებაში არ გამოიყენება) - ex: ........__.......__....
+print(type(True))
+print(type(False))
+print(5 > 0)
+print(5 < 0)
