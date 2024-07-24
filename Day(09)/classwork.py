d7674798ddb2ef07071ef277a5c47505fe0d3f39
Elene1029 <garibovielene69@gmail.com>
2024-07-24 17:51:34 +0400

    second commit

---
+++ Day(09)/classwork.py
@@ -0,0 +1,9 @@
+#score = 100
+#print(score < 120)
+#true or true = true
+#true or false = true
+#false or false = false
+#true and true = true
+#true and false = false
+#false and false = false
+
---
+++ Day(09)/classwork1.py
@@ -0,0 +1,17 @@
+print(True and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+
+
+print(1>0 and 2>1)
+print(10>5 and 10<5)
+print(3>10 and 5>10)
+
+print(1>0 or 2>1)
+print(10>5 or 10<5)
+print(3>10 or 5>10)
